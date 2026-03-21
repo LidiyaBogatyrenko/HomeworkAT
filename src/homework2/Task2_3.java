8722c9340f807edf6bb4a95c3786bb6ba8cc5aff
Lidiya_Bogatyrenko <lidia.bogatyrenko@gmail.com>
2026-03-21 21:12:27 +0300

    Added homework 2

--- src/homework2/Task2_3.java
+++ src/homework2/Task2_3.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 public class Task2_3 {
     /*
     Задача 3:
-Используя for вывести каждое четное число от 2 до 20 включительно и больше
-10
+Используя for вывести каждое четное число от 2 до 20 включительно и больше 10.
      */
     public static void main(String[] args) {
         for (int i = 10; i <= 20; i += 2) {
